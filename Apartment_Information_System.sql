a908c2e0d57c9c0b0787ced4b777938a2b78c750
D.Maciver <40775085+Arafat4869@users.noreply.github.com>
2020-09-18 20:12:21 +0600

    Updated Null field in Sql File

--- Apartment_Information_System.sql
+++ Apartment_Information_System.sql
@@ -17,7 +17,7 @@ Create Table Resident(
 	Flat_No varchar(10),
 	Contact_No varchar(20) unique ,
 	Email varchar(50) unique,
-	Permanent_Address varchar(100) not null,
+	Permanent_Address varchar(100),
 	Profession varchar(30) ,
 	Job_Address varchar(100),
 	Current_Living  bit not null,
@@ -152,7 +152,7 @@ Create Table Requests(
 
 INSERT INTO Resident(ResidentID, NID_BID, Name, Citizenship,Flat_No,Contact_No,Email,Permanent_Address,Profession,Job_Address,Current_Living,Pass) VALUES 
 ('1RID','3425678426', 'Shah Alam Tasin','True','A1','01921363894','shahalamtasin12@gmail.com','3/4 South Street Road,Mirpur 2,Dhaka','Software Engineer','Kanka Tower,Gulshan 2,Dhaka','True',HASHBYTES('MD5','01921363894')),
-('2RID','3859204934', 'Shams Al Ahsan','False','A1',' ','','',' ',' ','True',''),
+('2RID','3859204934', 'Shams Al Ahsan','False','A1','NULL','NULL','NULL','NULL','NULL','True','NULL'),
 ('3RID','2134234564', 'Arafat Uddin','True','A2','01745363894','arafatuddin21@gmail.com','22/C Polashi,Lalbagh,Dhaka','Gynecologist','Square Hospital,Panthapath,Dhaka','True',HASHBYTES('MD5','01745363894')),
 ('4RID','1943950476', 'Tashreef Mahmud','True','','01813637954','tashreef14@gmail.com','Doctor Goli,Malibagh,Dhaka','Teacher','Dhaka University,Dhaka','False',HASHBYTES('MD5','01813637954')),
 ('5RID','4325678532', 'Tasnid Swaad','True','D1','01642063894','shahalam.tasin@gmail.com','Comilla cantonment road,Comilla,Bangladesh','Actor','FDC Mor,Moghbazar','True',HASHBYTES('MD5','01642063894'))
@@ -182,11 +182,11 @@ INSERT INTO Manager(ManagerID,NID,Name,Contact_No,Permanent_Address,Email,Stat,P
 
 
 INSERT into ServiceProvider(SPID,Contact_No,Name,Present_Address,Permanent_Address,Designation) VAlUES
-('1SPID','01596347896','Sumon Sarker','7 No Road,Dhanmondi,Dhaka','','Home Tutor'),
+('1SPID','01596347896','Sumon Sarker','7 No Road,Dhanmondi,Dhaka','NULL','Home Tutor'),
 ('2SPID','01963216544','Sakhina Sultana','Cinema Hall Road,Farmgate,Dhaka','Abdullahpur,Gazipur','House Maid'),
 ('3SPID','01891231698','Jarina Khatun','Begun Bari,Tejgao,Dhaka','Horinath Road,Mymensingh','House Maid'),
-('4SPID','01778884949','Farhan Akter','Truck Stand,Tejgao,Dhaka','','DOT Network'),
-('5SPID','01896654932','Ali Mohammad','Railgate,Tejgao,Dhaka','','Ali Cable Network')
+('4SPID','01778884949','Farhan Akter','Truck Stand,Tejgao,Dhaka','NULL','DOT Network'),
+('5SPID','01896654932','Ali Mohammad','Railgate,Tejgao,Dhaka','NULL','Ali Cable Network')
 
 
 INSERT into OfficialPersonnel(DTID,ManagerID,Organization_Name,Reason,CountPeople,NID,Name,Contact_No,Profession,WorkInstitute)VALUES
@@ -195,11 +195,11 @@ INSERT into OfficialPersonnel(DTID,ManagerID,Organization_Name,Reason,CountPeopl
 
 
 INSERT into Flat(DTID,DTIN,DTOUT,Flat_No,ResidentID)VALUES
-('1600426484049FL','2020-09-18 16:54:44','','A1','1RID'),
-('1600426582146FL','2020-09-18 16:56:22','','A1','2RID'),
-('1600426628580FL','2020-09-18 16:57:08','','A2','3RID'),
+('1600426484049FL','2020-09-18 16:54:44','NULL','A1','1RID'),
+('1600426582146FL','2020-09-18 16:56:22','NULL','A1','2RID'),
+('1600426628580FL','2020-09-18 16:57:08','NULL','A2','3RID'),
 ('1600426671388FL','2020-09-18 16:57:51','2020-09-18 17:01:56','B3','4RID'),
-('1600426950197FL','2020-09-18 17:02:30','','D1','5RID')
+('1600426950197FL','2020-09-18 17:02:30','NULL','D1','5RID')
 
 
 INSERT into GuestEntry(DTID,SecurityID,ResidentID,GuestID)VALUES
@@ -219,7 +219,7 @@ INSERT into ServiceProviderEntry(DTID,SPID,SecurityID,Flat_No)VALUES
 
 INSERT into ServiceDuration(DTID,DTIN,Flat_No,SPID,DTOUT)VALUES
 ('1600428224139SD','2020-09-18 17:23:44','A1','1SPID','2020-09-18 17:46:54'),
-('1600429518842SD','2020-09-18 17:45:18','A2','2SPID',''),
+('1600429518842SD','2020-09-18 17:45:18','A2','2SPID','NULL'),
 ('1600429539094SD','2020-09-18 17:45:39','B3','3SPID','2020-09-18 17:47:03'),
 ('1600429556252SD','2020-09-18 17:45:56','D1','4SPID','2020-09-18 17:47:14')
 
@@ -232,9 +232,9 @@ INSERT into Fund(TransactionID,DateAndTime,TypeOfPayment,Amount_Paid,ResidentID)
 
 
 INSERT into Transactions(DTID,Flat_No,Paid_Amount,SPID,Report,ManagerID)VALUES
-('1600432352896TR','A1','300.00','5SPID','','2MID'),
-('1600432361699TR','A2','300.00','4SPID','','2MID'),
-('1600432373165TR','D1','300.00','5SPID','','2MID')
+('1600432352896TR','A1','300.00','5SPID','NULL','2MID'),
+('1600432361699TR','A2','300.00','4SPID','NULL','2MID'),
+('1600432373165TR','D1','300.00','5SPID','NULL','2MID')
 
 
 INSERT into Requests(DTID,ResidentID,ManagerID,Main_Request,Descrip,Urgency)VALUES
